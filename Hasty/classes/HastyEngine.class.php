e6c2e7cd07ee568923604de65081445e121b8b32
Lauri Orgla <>
2013-10-05 00:30:18 +0300

    removing functions for getting instance and replacing with class->get($class)

--- Hasty/classes/HastyEngine.class.php
+++ Hasty/classes/HastyEngine.class.php
@@ -9,12 +9,9 @@ class HastyEngine {
      */
     private $_instances = array();
 
-    /**
-     * 
-     */
     public function __construct() {
         require_once(Config::get('engine_path') . 'Autoloader.class.php');
-        $this->Autoloader();
+        $this->get('Autoloader');
     }
 
     /**
@@ -24,7 +21,7 @@ class HastyEngine {
      * @param type $class
      * @return boolean
      */
-    private function getInstance($class) {
+    private function get($class) {
         if (!key_exists($class, $this->_instances)) {
             if (class_exists($class, true)) {
                 $this->_instances[$class] = new $class();
@@ -37,61 +34,27 @@ class HastyEngine {
     }
 
     /**
-     * @author Lauri Orgla
-     * @version 1.0
-     * @return type
-     */
-    public function Request() {
-        return $this->getInstance(__FUNCTION__);
-    }
-
-    /**
-     * @author Lauri Orgla
-     * @version 1.0
-     * @return type
-     */
-    public function Processor() {
-        return $this->getInstance(__FUNCTION__);
-    }
-
-    /**
-     * @author Lauri Orgla
-     * @version 1.0
-     * @return type
-     */
-    public function Response() {
-        return $this->getInstance(__FUNCTION__);
-    }
-
-    /**
-     * Sets up autoloader for loading all classes automatically
-     * @author Lauri Orgla
-     * @version 1.0
-     * @return type
-     */
-    public function Autoloader() {
-        return $this->getInstance(__FUNCTION__);
-    }
-
-    /**
      * Runs the engine, gets requests and serves response
      * @author Lauri Orgla
      * @version 1.0
      */
     public function Run() {
-        $request = $this->Request()->parse();
-        $this->Response()->setRequest($request);
-        Authentication::authenticate(Request::data('api-token'));
+        $request = $this->get('Request')->parse();
+        $this->get('Response')->setRequest($request);
 
-        if (Config::get('authentication_enabled') == false || Authentication::authenticate(Request::data('api-token')) || (isset($request->route[0]->class) && in_array($request->route[0]->class, Config::get('public_controllers')))) {
-            $this->Autoloader()->appLoader($this->Request()->version);
-            $this->Processor()->process($this->Request());
+        $authenticated = false;
+        if (Request::data('api-token') != false) {
+            $authenticated = Authentication::authenticate(Request::data('api-token'));
+        } elseif (Request::data('api-key') != false && Request::data('api-secret') != false) {
+            $authenticated = Authentication::authenticateCredentials(Request::data('api-key'), Request::data('api-secret'));
+        }
 
-            $this->Response()->respond($this->Processor()->result);
+        if (Config::get('authentication_enabled') == false || $authenticated || (isset($request->route[0]->class) && in_array($request->route[0]->class, Config::get('public_controllers')))) {
+            $this->get('Autoloader')->appLoader($this->get('Request')->version);
+            $this->get('Processor')->process($this->get('Request'));
+            $this->get('Response')->respond($this->get('Processor')->result);
         } else {
-            $this->Response()->respond(array("msg" => "forbidden"));
-            /// has to be removed
-            //fallback to default response template
+            Response::forbidden();
         }
     }
 
